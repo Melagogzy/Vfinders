e701039b279c8dd531f3a5c7027fa8d22b512931
yourusername <melagogzbs@gmail.com>
2022-09-16 10:35:27 +0200

    First commit for auth pages, widgets and landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes,Route,  } from 'react-router-dom';
+
 import './App.css';
+import './mainStyle.css';
+import About from './Pages/About/Blocks/About'
+import Signup from './Pages/Signup';
+import SignIn from './Pages/Signin';
+import Homes from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Routes>
+              
+             <Route  path="/" element={<Home />} />
+             <Route path="/about" element={<About />} />
+             <Route path="/signup" element={<Signup />} />
+             <Route path="/signin" element={<SignIn />} />
+             
+              </Routes>
+         </BrowserRouter>
+       
+         
+
     </div>
   );
 }
